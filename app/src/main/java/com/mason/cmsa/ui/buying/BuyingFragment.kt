3401713374757b2239588d0d90bf9cc39de4b70c
10001049 <zonglei.huang@nflg.com>
2021-03-19 17:28:10 +0800

    【校园二手交易市场100】[feat]首页与求购的标题栏菜单；

--- app/src/main/java/com/mason/cmsa/ui/buying/BuyingFragment.kt
+++ app/src/main/java/com/mason/cmsa/ui/buying/BuyingFragment.kt
@@ -1,13 +1,12 @@
 package com.mason.cmsa.ui.buying
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.imyyq.mvvm.utils.ToastUtil
 import com.mason.cmsa.R
 
 class BuyingFragment : Fragment() {
@@ -15,17 +14,33 @@ class BuyingFragment : Fragment() {
     private lateinit var buyingViewModel: BuyingViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         buyingViewModel =
-                ViewModelProviders.of(this).get(BuyingViewModel::class.java)
+            ViewModelProviders.of(this).get(BuyingViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_buying, container, false)
         val textView: TextView = root.findViewById(R.id.text_notifications)
         buyingViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+        setHasOptionsMenu(true)
         return root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.buying, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.buying_i_request_to_buy -> {
+                ToastUtil.showShortToast("我要求购")
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
--- app/src/main/java/com/mason/cmsa/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mason/cmsa/ui/home/HomeFragment.kt
@@ -1,31 +1,52 @@
 package com.mason.cmsa.ui.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
 import android.widget.TextView
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.imyyq.mvvm.utils.ToastUtil
 import com.mason.cmsa.R
+
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+            ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+        setHasOptionsMenu(true)
         return root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.home, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.home_i_want_to_post -> {
+                ToastUtil.showShortToast("我要发布")
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
